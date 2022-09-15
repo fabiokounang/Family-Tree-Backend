85c28971ef5f5a99b26bf6d2004bf611eef2b0e9
fabiokounang11 <fabiokounang11@gmail.com>
2022-09-15 17:58:49 +0700

    setting cookie

--- helper-function/send-cookie.js
+++ helper-function/send-cookie.js
@@ -3,7 +3,8 @@ module.exports = (type, req, res, token, isLogout = false) => {
   const cookieOptions = {
     expires: expire,
     secure: req.secure || req.headers['x-forwarded-proto'] === 'https', // only https
-    httpOnly: true // cookies cannot be accessed or modified from the browser 
+    httpOnly: true, // cookies cannot be accessed or modified from the browser 
+    sameSite: 'none'
   }
   res.cookie('token' + type, token, cookieOptions);
 }
