4807425caeca0c9db49fc41328078a28e18cbaa0
fabiokounang11 <fabiokounang11@gmail.com>
2022-12-01 14:49:09 +0700

    add log

--- middleware/multer-excel.js
+++ middleware/multer-excel.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 const path = require('path');
-
+console.log(path.join(__dirname));
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, path.join(__dirname, '..', 'public'));
